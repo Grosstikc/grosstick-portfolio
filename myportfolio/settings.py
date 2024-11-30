2563d903d192678bfdc3286d0a25235ccd0b0f21
Rostyslav Shypita <rolikc19@gmail.com>
2024-11-30 10:22:44 +0100

    Collect statics update 2

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -127,7 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # WhiteNoise to serve static files in production
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
